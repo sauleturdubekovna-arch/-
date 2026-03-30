f2cd7872ebbc373441fda1553b5a6e1e711354e6
unknown <sauleturdubekovna@gmail.com>
2026-03-30 13:04:04 +0600

    Bermet +

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Medina from './components/Medina'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Medina/>
+     
     </>
   )
 }
---
+++ src/components/Bermet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Bermet() {
+  return (
+    <div>
+        <h1>Bermet</h1>
+    </div>
+  )
+}
+
+export default Bermet
